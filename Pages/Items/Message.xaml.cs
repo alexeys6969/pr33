737aa58f073064b0de3b184e767193582aafcf6f
Алексей Шашин <alexshashin.2023@gmail.com>
2026-02-14 22:26:22 +0500

    Разработанный пользовательский элемент Message. Шашин

---
+++ Pages/Items/Message.xaml.cs
@@ -0,0 +1,33 @@
+﻿using ChatStudentsShashin.Classes.Common;
+using ChatStudentsShashin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatStudentsShashin.Pages.Items
+{
+    /// <summary>
+    /// Логика взаимодействия для Message.xaml
+    /// </summary>
+    public partial class Message : UserControl
+    {
+        public Message(Messages Messages, User UserFrom)
+        {
+            InitializeComponent();
+            imgUser.Source = BitmapFromArrayByte.LoadImage(UserFrom.Photo);
+            FIO.Content = UserFrom.ToFio();
+            tbMessage.Text = Messages.Message;
+        }
+    }
+}
