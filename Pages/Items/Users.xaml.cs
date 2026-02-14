86fe33bae6c9a54cf5c57084c28e2d8c6d7606e6
Алексей Шашин <alexshashin.2023@gmail.com>
2026-02-14 22:21:05 +0500

    Разработанный пользовательский элемент User. Шашин

---
+++ Pages/Items/Users.xaml.cs
@@ -0,0 +1,41 @@
+﻿using ChatStudentsShashin.Classes.Common;
+using ChatStudentsShashin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatStudentsShashin.Pages.Items
+{
+    /// <summary>
+    /// Логика взаимодействия для Users.xaml
+    /// </summary>
+    public partial class Users : UserControl
+    {
+        User user;
+        Main main;
+        public Users(User user, Main main)
+        {
+            InitializeComponent();
+            this.user = user;
+            this.main = main;
+            imgUser.Source = BitmapFromArrayByte.LoadImage(user.Photo);
+            FIO.Content = user.ToFio();
+        }
+
+        private void SelectChat(object sender, MouseButtonEventArgs e)
+        {
+            main.SelectUser(user)
+        }
+    }
+}
