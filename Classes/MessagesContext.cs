0d1ff858856cb893363b9516a1f9d61c35d61e65
Алексей Шашин <alexshashin.2023@gmail.com>
2026-02-14 21:34:50 +0500

    Разработанные контексты данных. Шашин

---
+++ Classes/MessagesContext.cs
@@ -0,0 +1,20 @@
+﻿using ChatStudentsShashin.Classes.Common;
+using ChatStudentsShashin.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatStudentsShashin.Classes
+{
+    public class MessagesContext : DbContext
+    {
+        public DbSet<Messages> Messages { get; set; }
+        public MessagesContext() =>
+            Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlServer(Config.config);
+    }
+}
---
+++ Classes/UserContext.cs
@@ -0,0 +1,20 @@
+﻿using ChatStudentsShashin.Classes.Common;
+using ChatStudentsShashin.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatStudentsShashin.Classes
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users {  get; set; }
+        public UserContext() =>
+            Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlServer(Config.config);
+    }
+}
