300123d8498ba8b9b7f123acc50ea166b1ed2c8f
Алексей Шашин <alexshashin.2023@gmail.com>
2026-02-14 21:23:56 +0500

    Разработанные модели. Шашин

---
+++ Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatStudentsShashin.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Lastname { get; set; }
+        public string Firstname { get; set; }
+        public string Surname {  get; set; }
+        public byte[] Photo { get; set; }
+        public User(string Lastname, string Firstname, string Surname, byte[] Photo)
+        {
+            this.Lastname = Lastname;
+            this.Firstname = Firstname;
+            this.Surname = Surname;
+            this.Photo = Photo;
+        }
+        public string ToFio()
+        {
+            return $"{Lastname} {Firstname} {Surname}"
+        }
+
+    }
+}
