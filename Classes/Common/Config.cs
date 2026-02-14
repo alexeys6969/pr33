82d5419419b0bc839ef1fabfa8d54da5ddc2797a
Алексей Шашин <alexshashin.2023@gmail.com>
2026-02-14 21:29:06 +0500

    Разработанные общие классы. Шашин

---
+++ Classes/Common/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatStudentsShashin.Classes.Common
+{
+    public class Config
+    {
+        public static readonly string config = "Server=LAPTOP3019;Trusted_Connection=False;Database=;User=sa;Pwd=1111;"
+    }
+}
